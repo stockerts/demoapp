5e55b9d4df69fa7d4c64735da76b3f8a73621b30
stockerts <seanstockert@gmail.com>
2024-06-13 07:02:50 -0700

    Added client controller

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetClientIp()
+        {
+            var ipAddress = HttpContext.Connection.RemoteIpAddress?.ToString();
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                return NotFound("Unable to determine client IP address.");
+            }
+
+            return Ok(new { IpAddress = ipAddress });
+        }
+    }
+}
