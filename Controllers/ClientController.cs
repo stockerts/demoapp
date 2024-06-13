fbe4827f09fcbea54d6c0747c67b56c2503e4f3a
stockerts <seanstockert@gmail.com>
2024-06-13 07:22:10 -0700

    Updated client return, more details

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -9,13 +9,28 @@ namespace Client.Controllers
         [HttpGet]
         public IActionResult GetClientIp()
         {
+            // Get the client's IP address
             var ipAddress = HttpContext.Connection.RemoteIpAddress?.ToString();
-            if (string.IsNullOrEmpty(ipAddress))
-            {
-                return NotFound("Unable to determine client IP address.");
-            }
 
-            return Ok(new { IpAddress = ipAddress });
+            // Get the X-Forwarded-For header (if any)
+            var xForwardedFor = HttpContext.Request.Headers["X-Forwarded-For"].ToString();
+
+            // Get the User-Agent header
+            var userAgent = HttpContext.Request.Headers["User-Agent"].ToString();
+
+            // Get the Server header (if any)
+            var server = HttpContext.Request.Headers["Server"].ToString();
+
+            // Prepare the response object
+            var response = new
+            {
+                IpAddress = string.IsNullOrEmpty(ipAddress) ? "Unable to determine IP address." : ipAddress,
+                XForwardedFor = string.IsNullOrEmpty(xForwardedFor) ? "No X-Forwarded-For header present." : xForwardedFor,
+                UserAgent = userAgent,
+                ServerHeader = string.IsNullOrEmpty(server) ? "No Server header present." : server
+            };
+
+            return Ok(response);
         }
     }
 }
