f0c955f681d92f0df5465bdcb4b55f7d7387e469
“stockert” <seanstockert@gmail.com>
2025-06-19 14:29:09 -0700

    Adding API with minor updates.

---
+++ Controllers/MessageService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace demobankapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public class messageservice : ControllerBase
+    {
+        /// <summary>
+        /// Send a support message.
+        /// </summary>
+        /// <response code="200">Message successfully sent.</response>
+        // [Authorize]
+        [HttpPost("send")]
+        public IActionResult Send([FromBody] MessageModel messageSend)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(new
+            {
+                FirstName = messageSend.FirstName,
+                Email = messageSend.Email,
+                Message = messageSend.Message
+            });
+        }
+
+        public class MessageModel
+        {
+            [Required]
+            [RegularExpression("^[a-zA-Z0-9 ]+$", ErrorMessage = "Name must be alphanumeric")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [RegularExpression("^[a-zA-Z0-9 ]+$", ErrorMessage = "Name must be alphanumeric")]
+            public string LastName { get; set; }
+
+            [Required]
+            [RegularExpression(@"^\(\d{3}\) \d{3}-\d{4}$", ErrorMessage = "PhoneNumber format (###) ###-####")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            [EmailAddress(ErrorMessage = "Invalid email format")]
+            public string Email { get; set; }
+
+            [Required]
+            public string Message { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args); // Create new application builder
 
+builder.Services.AddControllers();  // Add services to the container.
 builder.Services.AddControllersWithViews(); // Adds support for MVC controllers and views
 builder.Services.AddHttpClient(); // Adds support for server-side requests
 
@@ -11,14 +15,38 @@ builder.Services.AddSession(options =>
     options.Cookie.Name = "demoapp_session"; // Changes the session cookie name from .AspNetCore.Session
 });
 
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Demo Bank API",
+        Description = "Use to create mock requests and responses. Not an official F5 property."
+    });
+
+    // Include XML comments
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 var app = builder.Build();  // Build the application
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo Bank API V1");
+    c.DefaultModelsExpandDepth(-1);  // Optional UI tweaks
+});
 
 app.UseStatusCodePagesWithReExecute("/Home/NotFound"); // Error redirect
 
 app.UseStaticFiles(); // Enable static file presentation like images
 app.UseRouting(); // Enable middleware routing
 app.UseSession(); // Enable session state
-
+app.MapControllers();
 
 app.MapControllerRoute(
 name: "default",
